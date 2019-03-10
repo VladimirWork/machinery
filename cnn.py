c481edf8972bf339733b3ba77ba7dace156bb767
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2019-03-10 22:55:14 +0300

    update
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- cnn.py
+++ cnn.py
@@ -1,8 +1,68 @@
+import os
+import shutil
 from keras import layers, models, optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+original_dataset_dir = 'C:\\Users\\admin\\Downloads\\train'
+base_dir = 'C:\\Users\\admin\\Downloads\\cnn_base_dir'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
 
 
-train_dir = 'C:\\Users\\admin\\Downloads\\train'
-test_dir = 'C:\\Users\\admin\\Downloads\\test'
+def setup():
+    os.mkdir(train_dir)
+    os.mkdir(validation_dir)
+    os.mkdir(test_dir)
+
+    train_cats_dir = os.path.join(train_dir, 'cats')
+    os.mkdir(train_cats_dir)
+    train_dogs_dir = os.path.join(train_dir, 'dogs')
+    os.mkdir(train_dogs_dir)
+    validation_cats_dir = os.path.join(validation_dir, 'cats')
+    os.mkdir(validation_cats_dir)
+    validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+    os.mkdir(validation_dogs_dir)
+    test_cats_dir = os.path.join(test_dir, 'cats')
+    os.mkdir(test_cats_dir)
+    test_dogs_dir = os.path.join(test_dir, 'dogs')
+    os.mkdir(test_dogs_dir)
+
+    fnames = ['cat.{}.jpg'.format(i)for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(train_cats_dir, fname)
+        shutil.copyfile(src, dst)
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(validation_cats_dir, fname)
+        shutil.copyfile(src, dst)
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(test_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(train_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(validation_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(test_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+
+
+# run only once
+setup()
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
@@ -17,3 +77,9 @@ model.add(layers.Flatten())
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer=optimizers.RMSprop(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+
+train_data_gen = ImageDataGenerator(rescale=1./255)
+test_data_gen = ImageDataGenerator(rescale=1./255)
+
+train_gen = train_data_gen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+test_gen = train_data_gen.flow_from_directory(test_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
