070dad6b42e1b2a9ba93149e0776c4b39f13277b
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2019-03-19 23:30:57 +0300

    update
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- cnn_visualization.py
+++ cnn_visualization.py
@@ -1,3 +1,4 @@
+from keras import models
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
@@ -14,4 +15,17 @@ img_tensor = np.expand_dims(img_tensor, axis=0)
 img_tensor /= 255.
 print(img_tensor.shape)
 plt.imshow(img_tensor[0])
+
+layer_outputs = [layer.output for layer in model.layers[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(img_tensor)
+first_layer_activation = activations[0]
+forth_layer_activation = activations[3]
+seventh_layer_activation = activations[6]
+print(first_layer_activation.shape)
+cmap = 'cool'
+plt.matshow(first_layer_activation[0, :, :, 4], cmap=cmap)
+plt.matshow(forth_layer_activation[0, :, :, 4], cmap=cmap)
+plt.matshow(seventh_layer_activation[0, :, :, 4], cmap=cmap)
+
 plt.show()
