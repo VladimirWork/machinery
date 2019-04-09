1f7565c34997ffc3af74ad9112ef0f2e5538767f
VladimirWork <vladimir.shishkin@dsr-corporation.com>
2019-04-09 22:51:53 +0300

    update
    Signed-off-by: VladimirWork <vladimir.shishkin@dsr-corporation.com>

--- !gan.py
+++ !gan.py
@@ -76,7 +76,40 @@ x_train = x_train.reshape((x_train.shape[0],) + (height, width, channels)).astyp
 
 iterations = 10000
 batch_size = 20
-save_dir = 'your_dir'
+save_dir = 'figures'
 start = 0
+
 for step in range(iterations):
-    raise NotImplementedError
+    # select random vectors from latent space
+    random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
+    # turn them into fake images
+    generated_images = generator.predict(random_latent_vectors)
+    # mix fake and real images
+    stop = start + batch_size
+    real_images = x_train[start: stop]
+    combined_images = np.concatenate([generated_images, real_images])
+    # gather labels for fake and real images
+    labels = np.concatenate([np.ones((batch_size, 1)), np.zeros((batch_size, 1))])
+    # hint: add noise to labels
+    labels += 0.05 * np.random.random(labels.shape)
+    # train discriminator
+    d_loss = discriminator.train_on_batch(combined_images, labels)
+    # select random vectors from latent space
+    random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
+    # gather false labels
+    misleading_targets = np.zeros((batch_size, 1))
+    # train generator
+    a_loss = gan.train_on_batch(random_latent_vectors, misleading_targets)
+
+    start += batch_size
+    if start > len(x_train) - batch_size:
+        start = 0
+    # save images every 100 steps
+    if step % 100 == 0:
+        gan.save_weights('gan.h5')
+        print('discriminator loss: {}'.format(d_loss))
+        print('adversarial loss: {}'.format(a_loss))
+        img = image.array_to_img(generated_images[0] * 255., scale=False)
+        img.save(os.path.join(save_dir, 'generated_frog' + str(step) + '.png'))
+        img = image.array_to_img(real_images[0] * 255., scale=False)
+        img.save(os.path.join(save_dir, 'real_frog' + str(step) + '.png'))
